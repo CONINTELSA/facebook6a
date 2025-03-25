455a2ce441761d9067a0635ef3e890e67c07ba84
jeffreymorales978 <112960355+jeffreymorales978@users.noreply.github.com>
2025-03-25 12:43:36 -0500

    Update create.php

--- app/views/sexo/create.php
+++ app/views/sexo/create.php
@@ -1,5 +1,5 @@
 <form action="../../controllers/SexoController.php?action=create" method="POST">
-    <label for="nombre">Nombre del estudiante:</label>
+    <label for="nombre">Nombre de la persona:</label>
     <input type="text" name="nombre" id="nombre" required>
     <input type="submit" value="Crear">
 </form>
